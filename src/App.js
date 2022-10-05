81a09a94cc92c7392b231ccc3e3810d86c222ad1
TedderGoGetter <103417758+TedderGoGetter@users.noreply.github.com>
2022-10-05 16:15:31 +0900

    added construction tape

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from './components/Header';
 // import TicTacToe from './components/TicTacToe';
+import Construction from './components/Construction'
 import Music from './components/Music'
 
 function App() {
   return (
     <div className="App">
+      <Construction/>
       <Header/>
       <Music/>
       {/* <TicTacToe/> */}
---
+++ src/components/Construction.js
@@ -0,0 +1,11 @@
+import './Construction.scss'
+
+const Construction = () => {
+  return (
+    <div className="tape">
+        <h3 classNAme="const">UNDER CONSTRUCTION</h3>
+    </div>
+  )
+}
+
+export default Construction
